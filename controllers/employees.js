802dadbf88141292ab735263250048107e8bfecb
cvargas306 <cavbcr@gmail.com>
2025-03-24 23:55:30 -0600

    Post, Put and Delete  methods added.

--- controllers/employees.js
+++ controllers/employees.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    //#swagger.tags=['contacts']
+    //#swagger.tags=['employees']
     const result = await mongodb.getDatabase().db().collection('employees').find();
     result.toArray().then((employees) => {
         res.setHeader('Content-Type', 'application/json');
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['contacts']
+    //#swagger.tags=['employees']
     const employeeId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('employees').find({ _id: employeeId });
     result.toArray().then((employees) => {
@@ -20,56 +20,56 @@ const getSingle = async (req, res) => {
     });
 };
 
-// const createContact = async (req, res) => {
-//     //#swagger.tags=['contacts']
-//     const contact = {
-//         firstName: req.body.firstName,
-//         lastName: req.body.lastName,
-//         email: req.body.email,
-//         favoriteColor: req.body.favoriteColor,
-//         birthday: req.body.birthday
-//     };
-//     const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
-//     if (response.acknowledged) {
-//         res.status(204).send();
-//     } else {
-//         res.status(500).json(response.error || 'Some error ocurred while adding the contact');
-//     }
-// };
+const createEmployee = async (req, res) => {
+    //#swagger.tags=['employees']
+    const employee = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('employees').insertOne(employee);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while adding employee');
+    }
+};
 
-// const updateContact = async (req, res) => {
-//     //#swagger.tags=['contacts']
-//     const contactId = new ObjectId(req.params.id);
-//     const contact = {
-//         firstName: req.body.firstName,
-//         lastName: req.body.lastName,
-//         email: req.body.email,
-//         favoriteColor: req.body.favoriteColor,
-//         birthday: req.body.birthday
-//     };
-//     const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact);
-//     if (response.modifiedCount > 0) {
-//         res.status(204).send();
-//     } else {
-//         res.status(500).json(response.error || 'Some error ocurred while updating the contact');
-//     }
-// };
+const updateEmployee = async (req, res) => {
+    //#swagger.tags=['employees']
+    const employeeId = new ObjectId(req.params.id);
+    const employee = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('employees').replaceOne({ _id: employeeId }, employee);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while updating employee');
+    }
+};
 
-// const deleteContact = async (req, res) => {
-//     //#swagger.tags=['contacts']
-//     const contactId = new ObjectId(req.params.id);
-//     const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId });
-//     if (response.deletedCount > 0) {
-//         res.status(204).send();
-//     } else {
-//         res.status(500).json(response.error || 'Some error ocurred while deleting the contact');
-//     }
-// };
+const deleteEmployee = async (req, res) => {
+    //#swagger.tags=['employees']
+    const employeeId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('employees').deleteOne({ _id: employeeId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while deleting employee');
+    }
+};
 
 module.exports = {
     getAll,
-    getSingle
-    // createContact,
-    // updateContact,
-    // deleteContact
+    getSingle,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee
 };
--- routes/employees.js
+++ routes/employees.js
@@ -7,10 +7,10 @@ router.get('/', employeesController.getAll);
 
 router.get('/:id', employeesController.getSingle);
 
-// router.post('/', employeesController.createEmployee);
+router.post('/', employeesController.createEmployee);
 
-// router.put('/:id', employeesController.updateEmployee);
+router.put('/:id', employeesController.updateEmployee);
 
-// router.delete('/:id', employeesController.deleteEmployee);
+router.delete('/:id', employeesController.deleteEmployee);
 
 module.exports = router;
