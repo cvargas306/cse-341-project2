fe32703f1bcde5311772c5e443d650f594bc87db
cvargas306 <cavbcr@gmail.com>
2025-03-29 22:24:24 -0600

    API swagger documentation added

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
 
 mongodb.initDb((err) => {
     if (err) {
