33a7c835147b4c595b9020896c7d3bfacc941d8e
cvargas306 <cavbcr@gmail.com>
2025-03-31 00:27:54 -0600

    Employee PUT method adjusted

--- controllers/employees.js
+++ controllers/employees.js
@@ -57,7 +57,7 @@ const createEmployee = async (req, res) => {
 const updateEmployee = async (req, res) => {
     //#swagger.tags=['employees']
     if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).json('Must use a valid employee id to update one.');
+        return res.status(400).json({ error: 'Must use a valid employee id to update one.' });
     }
     const employeeId = new ObjectId(req.params.id);
     const employee = {
