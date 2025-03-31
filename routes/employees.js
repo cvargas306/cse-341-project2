8df2eeffc16325e5f1f086c1446735cc69acde47
cvargas306 <cavbcr@gmail.com>
2025-03-31 08:51:15 -0600

    validation code adjusted. Swagger json edited

--- routes/employees.js
+++ routes/employees.js
@@ -10,7 +10,12 @@ router.get('/:id', employeesController.getSingle);
 
 router.post('/', validation.saveEmployee, employeesController.createEmployee);
 
-router.put('/:id', validation.saveEmployee, employeesController.updateEmployee);
+router.put('/:id', (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ error: 'Must use a valid employee id to update one.' });
+    }
+    next(); // Only proceed if ID is valid
+}, employeesController.updateEmployee);
 
 router.delete('/:id', employeesController.deleteEmployee);
 
